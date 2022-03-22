9419ce204ed6f7b6c963860cac0cc0fdeb46a7cf
achaayb <achaaybdev@gmail.com>
2022-03-22 14:05:34 +0100

    detailed structure

---
+++ data.js
@@ -0,0 +1,44 @@
+let foo = [
+    {
+        title : {
+            fr : "",
+            ar : ""
+        },
+        type : 0, // 0 = QCM
+        data : {
+            value1 : {
+                fr : "text1",
+                ar : "text2"
+            },
+            value1 : {
+                fr : "text1",
+                ar : "text2"
+            }
+        },
+        callback : foo
+    },
+    {
+        title : {
+            fr : "",
+            ar : ""
+        },
+        type : 1, // 1 = Number input
+        data : {
+            text : {
+                fr : "textfr",
+                ar : "textar"
+            },
+            min : 5,
+            max : 10
+        },
+        callback : bar
+    }
+];
+
+
+var foo = (q) => {
+
+    
+//display next :
+
+}
